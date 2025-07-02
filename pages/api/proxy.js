c6fb65233d12f9e2969d8a9b9f14776c81cd4746
Nayeem Hussain <127187936+NayeemHussainAds@users.noreply.github.com>
2025-07-02 12:18:35 +0600

    proxy.js

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -18,7 +18,7 @@ export default function handler(req, res) {
     res.end(`
       <html>
         <head>
-          <meta property="og:title" content="" />
+          <meta property="og:title" content="&#8203;" />
           <meta property="og:description" content="" />
           <meta property="og:image" content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4vyWL6EJrmdmh5IOmt7cGz-GpxdLFgF5qHJTMzRFF0Rma1014mNEKrY8xdgkl952JsVR8HHdYHrlS5lqyFOp79YYzz4EGXCBO7C1Ah1TjMXvu1_4P2d_Zgw5UhBk-fNBmCS32gT4QlbGAxh308MgcUbevLfYoeqdlJ8XnFnFHSs2QjDAiWodQLAMa4E0/w640-h360/Tiger%203%20(1).jpg" />
           <meta property="og:url" content="https://facebook.com/empty" />
